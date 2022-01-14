bca91133d80c3cd1e5485a8044f3f103a98f2407
juan_ <juan_@DESKTOP-M5ASF7R>
2022-01-14 18:50:30 -0500

    Agregar archivos de proyecto.

---
+++ Listas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<Alumnos>(); // El contenedor
+            for (int i = 1; i <= 10; i++)
+            {
+                var alumno = new Alumnos
+                {
+                    Id = i,
+                    Nombre = " Pepe " + i
+                };
+                lista.Add(alumno);
+            }
+
+
+            foreach (var a in lista)
+            {
+                Console.WriteLine($"El id es : {a.Id} el nombre es:{a.Nombre} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
